17cf6cd9c202a81c55dfd009d5ba155d316a0dfe
Hang <whitewield@126.com>
2018-03-04 19:52:41 -0500

    show beat feedback

---
+++ Develop/DungeonDoubleDance/Assets/Scripts/CS_BeatFeedback.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Global;
+
+public class CS_BeatFeedback : MonoBehaviour {
+
+
+	[System.Serializable]
+	public struct BeatFeedbackSetup {
+		public float myEndRate;
+		public string myText;
+		public Color myColor;
+	}
+
+	[SerializeField] Text myText;
+	[SerializeField] float myMaxTime = 1;
+	private float myMaxTimeMultiplier;
+	private float myTimer;
+	[SerializeField] AnimationCurve mySizeOverTime;
+	[SerializeField] AnimationCurve myAlphaOverTime;
+	[SerializeField] BeatFeedbackSetup[] mySetups;
+
+	void Awake () {
+		myText.text = "";
+		myMaxTimeMultiplier = 1 / myMaxTime;
+	}
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (myTimer <= 0)
+			return;
+
+		myTimer -= Time.deltaTime;
+		if (myTimer <= 0) {
+			myTimer = 0;
+		}
+
+		float t_process = 1 - (myTimer * myMaxTimeMultiplier);
+
+		myText.transform.localScale = mySizeOverTime.Evaluate (t_process) * Vector3.one;
+
+		Color t_color = myText.color;
+		t_color.a = myAlphaOverTime.Evaluate (t_process);
+		myText.color = t_color;
+	}
+
+	public void Show (float g_rate) {
+		for (int i = 0; i < mySetups.Length; i++) {
+			if (g_rate > mySetups [i].myEndRate) {
+				Show (mySetups [i]);
+				return;
+			}
+		}
+
+		Show (mySetups [mySetups.Length - 1]);
+	}
+
+	public void Show (BeatFeedbackSetup g_setup) {
+		myText.text = g_setup.myText;
+		myText.color = g_setup.myColor;
+		myTimer = myMaxTime;
+	}
+}
--- Develop/DungeonDoubleDance/Assets/Scripts/CS_PlayerController.cs
+++ Develop/DungeonDoubleDance/Assets/Scripts/CS_PlayerController.cs
@@ -1,24 +1,38 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using Global;
 using JellyJoystick;
 
 public class CS_PlayerController : CS_Controller {
 
+	[SerializeField] Transform myBar_Transform;
+	private float myBar_ValueMax = 100;
+	private float myBar_ValueCurrent;
+	[SerializeField] float myBar_ValueDecreaseSpeed = 10;
+	[SerializeField] float myBar_ValueOnBeatMultiplier = 10;
+	[SerializeField] float myBar_ValueOffBeatDecrease = 20;
+
 	private int myJoystickNumber = 1;
+
+	[SerializeField] CS_BeatFeedback myBeatFeedback;
 
 	protected override void Init () {
 		myBattlefieldSide = (BattlefieldSide)(myJoystickNumber - 1);
 		CS_GameManager.Instance.SetMyController (this, myBattlefieldSide);
 
 		Init_Heros (CS_PlayerManager.Instance.GetHeroSetups ());
+
+		UpdateBarDisplay ();
 	}
 
 	protected override void Update () {
 		base.Update ();
 
 		Update_Input ();
+
+		Update_Bar ();
 	}
 
 	protected void Update_Input () {
@@ -37,7 +51,38 @@ public class CS_PlayerController : CS_Controller {
 		if (JellyJoystickManager.Instance.GetButton (ButtonMethodName.Down, myJoystickNumber, JoystickButton.Y)) {
 			OnKey (Key.Y);
 		}
+	}
 
+	protected override void DoOnBeat () {
+		float t_accuracy = CS_RhythmManager.Instance.GetAccuracy ();
+//		Debug.LogWarning ("DoOnBeat" + t_accuracy);
+		myBeatFeedback.Show (t_accuracy);
+		ModifyBarValue (t_accuracy * t_accuracy * myBar_ValueOnBeatMultiplier);
+	}
+
+	protected override void DoOffBeat () {
+		float t_accuracy = CS_RhythmManager.Instance.GetAccuracy ();
+//		Debug.LogWarning ("OOOOOOOOOOOFF" + t_accuracy);
+		myBeatFeedback.Show (-1);
+		ModifyBarValue (-myBar_ValueOffBeatDecrease);
+	}
+
+	private void ModifyBarValue (float g_value) {
+		myBar_ValueCurrent += g_value;
+		if (myBar_ValueCurrent > myBar_ValueMax)
+			myBar_ValueCurrent = myBar_ValueMax;
+		else if (myBar_ValueCurrent < 0)
+			myBar_ValueCurrent = 0;
+	}
+
+	private void Update_Bar () {
+		ModifyBarValue (-Time.deltaTime * myBar_ValueDecreaseSpeed);
+		UpdateBarDisplay ();
+	}
+
+	private void UpdateBarDisplay () {
+		myBar_Transform.localScale = new Vector3 (myBar_ValueCurrent / myBar_ValueMax, 1, 1);
 	}
 
 }
+
